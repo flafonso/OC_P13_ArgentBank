bf89cb8e405fef5ce2dd2e393e1ef6e520d407ac
Flavien Afonso <af.flavien@gmail.com>
2024-07-09 16:12:21 +0200

    Adding footer content

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 function Footer() {
   return (
-    <footer className="footer">Footer</footer>
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
   );
 }
 
