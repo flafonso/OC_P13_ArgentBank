b53fcd33ac15a1023e8efd35da9f3a1b4db73032
Flavien Afonso <af.flavien@gmail.com>
2024-07-09 16:11:09 +0200

    Adding nav bar content

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav className="main-nav">main nav</nav>
+    <nav className="main-nav">
+      <Link to="/">
+        <img
+          className="main-nav-logo-image"
+          src="/img/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <Link to="/" className="main-nav-item">
+          <i className="fa fa-user-circle"></i> Sign In
+        </Link>
+      </div>
+    </nav>
   );
 }
 
