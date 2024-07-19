d515682413d3c8120631a098d1921e0ff1e90965
Flavien Afonso <af.flavien@gmail.com>
2024-07-19 05:16:46 +0200

    Added form field validation and error handling to ProfileHeader component, and updated CSS to style error inputs and messages

--- frontend/src/features/profileHeader/ProfileHeader.tsx
+++ frontend/src/features/profileHeader/ProfileHeader.tsx
@@ -2,11 +2,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../../app/store";
 import { useState } from "react";
 import { updateProfile } from "../authentication/authenticationSlice";
+import { editFormFieldCheck, FormFieldCheck } from "../../utils/fieldCheck";
 
 function ProfileHeader() {
   const userProfile = useSelector((state: RootState) => state.auth.userProfile);
-  console.log(userProfile);
+  // console.log(userProfile);
   const [showEdit, setshowEdit] = useState(false);
+  const [fieldCheck, setFieldCheck] = useState<FormFieldCheck>({
+    valid: false,
+    error: {},
+  });
   const dispatch = useDispatch<AppDispatch>();
 
   const handleEdit = () => {
@@ -18,20 +23,29 @@ function ProfileHeader() {
   const handleSave = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
-    const firstName = data.get("firstName") as string;
-    const lastName = data.get("lastName") as string;
+    const firstName = (data.get("firstName") as string | null)?.trim() ?? "";
+    const lastName = (data.get("lastName") as string | null)?.trim() ?? "";
+    // console.log("check dans le composant retour direct =>");
+    // console.log(editFormFieldCheck(firstName, lastName));
 
+    const validation = editFormFieldCheck(firstName, lastName);
+    setFieldCheck(validation);
+    console.log("check dans le composant =>");
+    console.log(fieldCheck);
+
+    if (!validation.valid) {
+      return;
+    }
     try {
-      const resultUpdate = await dispatch(updateProfile({ firstName, lastName }));
+      const resultUpdate = await dispatch(
+        updateProfile({ firstName, lastName })
+      );
       if (updateProfile.fulfilled.match(resultUpdate)) {
-        console.log("update réussi");
-      } else {
-        console.log("update échoué");
+        setshowEdit(false);
       }
     } catch (error) {
       console.error("Failed to login:", error);
     }
-    setshowEdit(false);
   };
 
   return (
@@ -45,16 +59,32 @@ function ProfileHeader() {
       {showEdit ? (
         <form className="edit-form" onSubmit={handleSave}>
           <div className="edit-inputs">
-            <input
-              type="text"
-              name="firstName"
-              placeholder={userProfile?.firstName}
-            />
-            <input
-              type="text"
-              name="lastName"
-              placeholder={userProfile?.lastName}
-            />
+            <div>
+              <input
+                type="text"
+                name="firstName"
+                placeholder={userProfile?.firstName}
+                className={fieldCheck.error.firstName ? "error" : ""}
+              />
+              {fieldCheck.error.firstName && (
+                <div className="error-message">
+                  {fieldCheck.error.firstName}
+                </div>
+              )}
+            </div>
+            <div>
+              <input
+                type="text"
+                name="lastName"
+                placeholder={userProfile?.lastName}
+                className={fieldCheck.error.lastName ? "error" : ""}
+              />
+              {fieldCheck.error.lastName && (
+                <div className="error-message">
+                  {fieldCheck.error.lastName}
+                </div>
+              )}
+            </div>
           </div>
           <div className="edit-buttons">
             <button type="submit" className="save-button">
